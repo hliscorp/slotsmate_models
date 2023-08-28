bfe8803a2cbcf6a30789f8d10b8a5db7298f6224
biancaadd <60610944+biancaadd@users.noreply.github.com>
2023-08-28 14:33:11 +0300

    fx

--- src/Entities/Author/Author.php
+++ src/Entities/Author/Author.php
@@ -5,14 +5,14 @@ namespace Hlis\SlotsMateModels\Entities\Author;
 class Author extends \Entity
 {
 
-    public ?int $id;
-    public ?string $name;
-    public ?string $date_joined;
-    public ?string $expertise;
-    public ?string $highlights; // prev short_bio
-    public ?string $full_bio;
-    public ?string $tagline; // prev role (string) but now it is based on locale_id
-    public ?array $social_networks;
+    public ?int $id = null;
+    public ?string $name = null;
+    public ?string $date_joined = null;
+    public ?string $expertise = null;
+    public ?string $highlights = null; // prev short_bio
+    public ?string $full_bio = null;
+    public ?string $tagline = null; // prev role (string) but now it is based on locale_id
+    public ?array $social_networks = [];
     // public ?array $games;
     // public ?array $articles;
 
