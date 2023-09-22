2a80d97b0cd294ebe5b16cdc1aa5cf10b2588ea7
Lewis1727 <59251780+Lewis1727@users.noreply.github.com>
2023-09-22 18:05:59 +0300

    MATE-19020 3rd point

--- src/Queries/Author/ConditionsSetter/AuthorConditions.php
+++ src/Queries/Author/ConditionsSetter/AuthorConditions.php
@@ -31,7 +31,14 @@ class AuthorConditions extends AbstractConditions
 
     protected function setDisabledCondition(Condition $condition): void
     {
-        $is_disabled = $this->filter->getDisabledStatus() ?? "0";
+        if($this->filter->getDisabledStatus() === null) return;
+
+        if($this->filter->getDisabledStatus() === true) {
+            $is_disabled = 1;
+        } else {
+            $is_disabled = 0;
+        }
+        
         $condition->set("t1.disabled", $is_disabled);
     }
 
