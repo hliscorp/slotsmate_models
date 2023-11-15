329ec42978956faed0a6a03c03657f15d700d2ba
frstshmn <korenha.v@gmail.com>
2023-11-15 17:41:39 +0200

    MATE-19236

--- src/Queries/BankingMethods/ConditionsSetter/ListConditionsSetter.php
+++ src/Queries/BankingMethods/ConditionsSetter/ListConditionsSetter.php
@@ -14,6 +14,7 @@ class ListConditionsSetter extends BankingMethodConditions
 
         $this->setCasinosCondition($condition);
         $this->setExcludedIdCondition($condition);
+        $this->setSelectedCountryCondition($condition);
     }
 
     protected function setCasinosCondition(Condition $condition): void
