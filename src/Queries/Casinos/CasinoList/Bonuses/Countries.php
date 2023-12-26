b5b5a19df817f187e0f26eecf1519e5439a6881c
shaykeman <shay@kalaih.com>
2023-12-26 17:23:46 +0200

    MATE-19101

--- src/Queries/Casinos/CasinoList/Bonuses/Countries.php
+++ src/Queries/Casinos/CasinoList/Bonuses/Countries.php
@@ -12,7 +12,7 @@ class Countries extends Query
         $this->query = new \Lucinda\Query\Select("casinos__bonuses_countries", "t1");
         $this->setFields($this->query->fields());
         $this->query->joinInner("countries", "t2")->on(["t1.country_id"=>"t2.id"]);
-        $this->query->where()->setIn("t1.casino_bonus_id", $ids);
+        $this->query->where()->setIn("t1.casino_bonus_id", $ids)->set("t2.keep", 1);
     }
 
     private function setFields(Fields $fields): void
