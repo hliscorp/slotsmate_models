6d1cb50043eb6ee00de630d4e4f3b828ca06c517
Lewis1727 <daniil2003478@gmail.com>
2024-03-05 13:50:45 +0100

    MATE-24247

--- src/Queries/Casinos/CasinoInfo/WithdrawMethods.php
+++ src/Queries/Casinos/CasinoInfo/WithdrawMethods.php
@@ -12,12 +12,13 @@ class WithdrawMethods extends DefaultWithdrawMethods
     {
         $fields->add("t1.banking_method_id", "id");
         $fields->add("t2.name");
+        $fields->add("IF(lb.id IS NOT NULL,1,0) AS is_locale");
     }
     
     protected function setJoins(): void
     {
         parent::setJoins();
-        $this->query->joinInner("locale__banking", "lb")->on(["lb.banking_id"=>"t2.id"]);
+        $this->query->joinLeft("locale__banking", "lb")->on(["lb.banking_id"=>"t2.id"]);
         if($this->filter->getLocaleCountry()) {
             $this->query->joinInner('banking_methods__countries_allowed', 't3')->on([
                 "t1.banking_method_id" => "t3.banking_method_id",
