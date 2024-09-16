b8ccd40c97ad463d7999f463684c868007b2dbff
Maksym Hanyn <maksym.hanyn@jetsoftpro.com>
2024-09-16 13:23:57 +0300

    fix response randomizing

--- src/Queries/LearnArticles/SortBy/LearnArticleSortBy.php
+++ src/Queries/LearnArticles/SortBy/LearnArticleSortBy.php
@@ -13,9 +13,12 @@ class LearnArticleSortBy extends AbstractOrderBy
         switch ($orderByAlias) {
             case LearnArticleCriteria::DATE_CREATED:
                 $this->orderBy->add("t1.date_added", OrderBy::DESC);
+                $this->orderBy->add("t1.id", OrderBy::DESC);
                 break;
             case LearnArticleCriteria::PRIORITY:
                 $this->orderBy->add("t1.priority");
+                $this->orderBy->add("t1.date_added", OrderBy::DESC);
+                $this->orderBy->add("t1.id", OrderBy::DESC);
                 break;
             default:
                 throw new \InvalidArgumentException("Invalid sort criteria: " . $orderByAlias);
