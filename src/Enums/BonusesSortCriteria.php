e05f83a1ca390698d4f2655f693049f683a00004
DmytroSan <dmytro.s@gtmarketing.ro>
2024-11-12 13:26:09 +0200

    MATE-32177

--- src/Enums/BonusesSortCriteria.php
+++ src/Enums/BonusesSortCriteria.php
@@ -4,6 +4,7 @@ namespace Hlis\SlotsMateModels\Enums;
 
 class BonusesSortCriteria
 {
+    public const NEWEST = 0;
     public const GAMES_BONUSES = 1;
     public const IS_ALLOWED_IN_COUNTRY_NEWEST = 2;
 }
