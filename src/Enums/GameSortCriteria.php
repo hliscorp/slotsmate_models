6caa649b30c65eca1780d1ed99cde7713a11a1a1
DmytroSan <dmytro.s@gtmarketing.ro>
2025-01-07 11:24:55 +0200

    MATE-30416 - clearing GameSortCriteria

--- src/Enums/GameSortCriteria.php
+++ src/Enums/GameSortCriteria.php
@@ -11,17 +11,10 @@ class GameSortCriteria
     const RTP = 5;
     const POPULAR = 6;
     const COUNTER = 7;
-    const FLASH = 7;
+
     const DATE_ASC = 8;
     const BEST = 9;
 
-    // const NONE = 1;
-    // const NEWEST = 2;
-    // const BEST = 3;
-    // const MOST_PLAYED = 4;
-    // const RTP = 5;
-    // const COUNTER = 6;
-    // const DATE_ASC = 7;
     const DEMO = 10;
     const RELEVANT = 11;
     const DATE_DESC = 12;
